3636a9c24f10a7ca93a8f53e014dd14286091d0d
Johan Swetzén <johan.swetzen@cybercom.com>
2016-12-08 07:58:03 +0100

    Eighth problem solved

---
+++ p8.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+INPUT = 'input8.txt'
+
+from common import splitLines, matchRegex
+
+screen = [[0 for x in range(50)] for y in range(6)]
+
+def add_rect(screen, x, y):
+    for i in range(x):
+        for j in range(y):
+            screen[j][i] = 1
+    return screen
+
+def shift(ary, by):
+    result = [0 for x in range(len(ary))]
+    for i in range(len(ary)):
+        result[(i+by) % len(ary)] = ary[i]
+    return result
+
+def rotate(screen, row, index, by):
+    if row:
+        screen[index] = shift(screen[index], by)
+    else:
+        column = [screen[x][index] for x in range(len(screen))]
+        column = shift(column, by)
+        for x in range(len(screen)):
+            screen[x][index] = column[x]
+            
+    return screen
+
+def count_lit(screen):
+    count = 0
+    for row in screen:
+        count += sum(row)
+    return count
+
+def prettyprint(screen):
+    for line in screen:
+        print(''.join(['#' if c == 1 else ' ' for c in line]))
+
+def task1():
+    for line in splitLines(INPUT, ' '):
+        if line[0] == 'rect':
+            [x,y] = line[1].split('x')
+            add_rect(screen, int(x), int(y))
+        elif line[0] == 'rotate':
+            row = line[1] == 'row'
+            index = int(line[2].split('=')[1])
+            by = int(line[4])
+            rotate(screen, row, index, by)
+
+    print(count_lit(screen))
+
+def task2():
+    prettyprint(screen)
+    print("AFBUPZBJPS")
+
+task1()
+task2()
