49e86f1d44144e0a1c84ea581717e63f03dfa29e
Johan Swetzén <johan.swetzen@cybercom.com>
2016-12-16 21:45:19 +0100

    Shorter sixteenth solution

--- p16.py
+++ p16.py
@@ -7,25 +7,21 @@ test_input = '10000'
 test_len = 20
 
 def dragon(binary):
-    binary2 = ''.join([ '1' if x is '0' else '0' for x in reversed(binary) ])
-    return binary + '0' + binary2
+    binary2 = ([0 if x else 1 for x in binary[::-1]])
+    return binary + [0] + binary2
 
-def expand(binary, length):
+def expand(binstr, length):
+    binary = list(map(int, binstr))
     while len(binary) < length:
         binary = dragon(binary)
 
     return binary[:length]
 
 def checksum(b):
-    while len(b) % 2 is 0:
-        newb = ''
-        for k,v in enumerate(b[:-1]):
-            if k % 2 == 1:
-                continue
-            if v == b[k+1]:
-                newb += '1'
-            else:
-                newb += '0'
+    while not (len(b) & 1):
+        newb = []
+        for i in range(int(len(b) / 2)):
+            newb.append(1 if b[2*i] == b[2*i+1] else 0)
         b = newb
     return b
 
@@ -37,6 +33,6 @@ def task2():
 
 if __name__ == '__main__':
 
-    print(task1())
-    print(task2())
+    print(''.join(map(str,task1())))
+    print(''.join(map(str,task2())))
 
